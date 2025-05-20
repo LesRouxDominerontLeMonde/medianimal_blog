bfe5eaa8bfecd78674040e70a99a8578eea0edac
LesRouxDominerontLeMonde <105645582+LesRouxDominerontLeMonde@users.noreply.github.com>
2025-05-20 15:38:56 +0200

    Remove Trumbowyg minified JS file, update todo with installation notes for Webpack Encore and Trumbowyg, and add a new entry for admin in Webpack configuration.

--- assets/admin.js
+++ assets/admin.js
@@ -2,6 +2,6 @@ import $ from 'jquery';
 import 'trumbowyg';
 import 'trumbowyg/dist/ui/trumbowyg.min.css';
 
-$(document).ready(function () {
-    $('.trumbowyg-editor').trumbowyg();
+$(function () {
+  $('.trumbowyg-editor').trumbowyg();
 });
