8f3beb9fbb05eb0f48a31ade2c8b8b7a6b82d474
LesRouxDominerontLeMonde <105645582+LesRouxDominerontLeMonde@users.noreply.github.com>
2025-05-26 10:39:39 +0200

    ajout image dans article.php et BDD

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -26,6 +26,9 @@ class Article
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $publishedAt = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +78,18 @@ class Article
     public function setPublishedAt(?\DateTimeImmutable $publishedAt): static
     {
         $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
